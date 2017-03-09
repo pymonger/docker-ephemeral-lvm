39584445000578b44618e4aaf078ae7b782a4ed1
pymonger <pymonger@gmail.com>
2017-03-09 15:14:39 +0000

    provision encrypted hysds data dir if ephemeral disk 0 is mounted as mnt

--- docker-ephemeral-lvm.d/docker-ephemeral-lvm.sh
+++ docker-ephemeral-lvm.d/docker-ephemeral-lvm.sh
@@ -54,3 +54,46 @@ if [[ -e "$DEV" ]]; then
 fi
 
 systemctl start docker
+
+# Setup Instance Store 0 for HySDS work dir (/data) if mounted as /mnt
+DATA_DIR="/data"
+DATA_DEV="/dev/xvdb"
+if grep -s $DATA_DEV /proc/mounts | grep -qs /mnt; then
+  # clean out /mnt, /data and /data.orig
+  rm -rf /mnt/cache /mnt/jobs /mnt/tasks
+  rm -rf /data/work/cache /data/work/jobs /data/work/tasks
+  rm -rf /data.orig
+
+  # backup /data/work and index style
+  cp -rp /data /data.orig || true
+
+  # unmount block device if not already
+  umount $DATA_DEV 2>/dev/null || true
+
+  # install cryptsetup
+  yum install -y cryptsetup || true
+
+  # generate random passphrase
+  PASSPHRASE=`hexdump -n 16 -e '4/4 "%08X" 1 "\n"' /dev/random`
+
+  # format the ephemeral volume with selected cipher
+  echo $PASSPHRASE | cryptsetup luksFormat -c twofish-xts-plain64 -s 512 --key-file=- $DATA_DEV
+
+  # open the encrypted volume to a mapped device
+  echo $PASSPHRASE | cryptsetup luksOpen --key-file=- $DATA_DEV ephemeral-encrypted-data
+
+  # set name of mapped device
+  DATA_DEV_ENC="/dev/mapper/ephemeral-encrypted-data"
+
+  # format XFS
+  mkfs.xfs $DATA_DEV_ENC
+
+  # mount as /data
+  mount $DATA_DEV_ENC $DATA_DIR
+
+  # set permissions
+  chown -R ops:ops /data || true
+
+  # copy work and index style
+  cp -rp /data.orig/work /data/ || true
+fi
