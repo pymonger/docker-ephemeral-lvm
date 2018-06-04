a872cf6d6e6128ba8a9c8efd75313954ebe49fd2
Gerald Manipon <pymonger@gmail.com>
2018-06-04 18:40:13 +0000

    set default script to not encrypt volumes; rely on cloud platform to do the volume encryption

--- docker-ephemeral-lvm.d/docker-ephemeral-lvm.sh
+++ docker-ephemeral-lvm.d/docker-ephemeral-lvm.sh
@@ -140,27 +140,12 @@ if [[ -e "$DATA_DEV" ]]; then
   # unmount block device if not already
   umount $DATA_DEV 2>/dev/null || true
 
-  # install cryptsetup
-  yum install -y cryptsetup || true
-
-  # generate random passphrase
-  PASSPHRASE=`hexdump -n 16 -e '4/4 "%08X" 1 "\n"' /dev/random`
-
-  # format the ephemeral volume with selected cipher
-  echo $PASSPHRASE | cryptsetup luksFormat -c twofish-xts-plain64 -s 512 --key-file=- $DATA_DEV
-
-  # open the encrypted volume to a mapped device
-  echo $PASSPHRASE | cryptsetup luksOpen --key-file=- $DATA_DEV ephemeral-encrypted-data
-
-  # set name of mapped device
-  DATA_DEV_ENC="/dev/mapper/ephemeral-encrypted-data"
-
   # format XFS
-  mkfs.xfs -f $DATA_DEV_ENC
+  mkfs.xfs -f $DATA_DEV
 
   # mount as ${DATA_DIR}
   mkdir -p $DATA_DIR || true
-  mount $DATA_DEV_ENC $DATA_DIR
+  mount $DATA_DEV $DATA_DIR
 
   # create work and unpack index style
   mkdir -p ${DATA_DIR}/work || true
@@ -189,27 +174,12 @@ if [[ -e "$DOCKER_DEV" ]]; then
   # remove physical volume
   pvremove -ff $DOCKER_DEV || true
 
-  # install cryptsetup
-  yum install -y cryptsetup || true
-
-  # generate random passphrase
-  PASSPHRASE=`hexdump -n 16 -e '4/4 "%08X" 1 "\n"' /dev/random`
-
-  # format the ephemeral volume with selected cipher
-  echo $PASSPHRASE | cryptsetup luksFormat -c twofish-xts-plain64 -s 512 --key-file=- $DOCKER_DEV
-
-  # open the encrypted volume to a mapped device
-  echo $PASSPHRASE | cryptsetup luksOpen --key-file=- $DOCKER_DEV ephemeral-encrypted
-
-  # set name of mapped device
-  DOCKER_DEV_ENC="/dev/mapper/ephemeral-encrypted"
-
   # determine 75% of volume size to be used for docker data
   DATA_SIZE=`lsblk -b $DOCKER_DEV | grep disk | awk '{printf "%.0f\n", $4/1024^3*.75}'`
 
   # create physical volume and volume group for docker
-  pvcreate -ff $DOCKER_DEV_ENC
-  vgcreate -ff  vg-docker $DOCKER_DEV_ENC
+  pvcreate -ff $DOCKER_DEV
+  vgcreate -ff  vg-docker $DOCKER_DEV
 
   # reconfigure docker storage for devicemapper
   echo "STORAGE_DRIVER=devicemapper" > /etc/sysconfig/docker-storage-setup
